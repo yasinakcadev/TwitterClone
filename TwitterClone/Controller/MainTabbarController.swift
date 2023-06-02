841a3089d0ee81d710058b33f31870fe6b9162aa
yasinakcadev <yasinakcadev@gmail.com>
2023-06-03 00:42:43 +0300

    project setup

---
+++ TwitterClone/Controller/MainTabbarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabbarController.swift
+//  TwitterClone
+//
+//  Created by YASIN on 3.06.2023.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
